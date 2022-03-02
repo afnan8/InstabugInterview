9360842847ba646cd9442654d4a6a09eafde0c9f
afnan8 <afnan.h.mohamed@gmail.com>
2022-03-02 09:47:50 +0200

    data extension

--- InstabugNetworkClient/Helpers/Data+Extension.swift
+++ InstabugNetworkClient/Helpers/Data+Extension.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension Data {
+    
+    func convertToMB() -> Double {
+        let bcf = ByteCountFormatter()
+        bcf.allowedUnits = [.useMB]
+        bcf.countStyle = .file
+        let sizeString = bcf.string(fromByteCount: Int64(self.count))
+        guard let size = Double(sizeString.replacingOccurrences(of: " MB", with: "")) else { return 0}
+        return size
+    }
+}
